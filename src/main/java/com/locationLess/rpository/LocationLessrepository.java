1bb023e75d78ade007a6fca40b41cc9fd4e71ba1
paramsd1997 <111410267+paramsd1997@users.noreply.github.com>
2022-09-25 12:12:16 +0530

    Delete LocationLessrepository.java

--- src/main/java/com/locationLess/rpository/LocationLessrepository.java
+++
@@ -1,9 +0,0 @@
-package com.locationLess.rpository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.locationLess.entities.LocationLess;
-
-public interface LocationLessrepository extends JpaRepository<LocationLess, Long> {
-
-}
